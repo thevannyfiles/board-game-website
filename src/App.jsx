c53361c0851f5f16a4752eac9e49a3baaf4c4b43
Darnel Castor <darnelcastor2@gmail.com>
2024-07-12 09:45:26 -0400

    Added hero to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Ourstory from "./pages/ourstory.jsx";
 import Header from "./Components/Header.jsx";
-
+import Hero from "./Components/Hero.jsx";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <div>
           <Header />
         </div>
-      </BrowserRouter>
+        <Routes>
+          <Route path="/about" element={<Ourstory />} />
+        </Routes>
+      </Router>
+      <div>
+        <Hero />
+      </div>
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,73 +1,218 @@
-import React, { useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
-const Header = () => {
-  const [nav, setNav] = useState(false);
-  const handleNav = () => {
-    setNav(!nav);
-  };
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <div>
-      <div>
-        <a href="">
-          <span></span>
-        </a>
-      </div>
-      <ui>
-        <li>
-          <NavLink to="/" activeClassName="selected">
-            Our Story
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/" activeClassName="selected">
-            {" "}
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/" activeClassName="selected">
-            {" "}
-            Portfolio{" "}
-          </NavLink>
-        </li>
-      </ui>
-      <div onClick={handleNav} className="block md:hidden text-white z-40">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <Link to="/">
+                <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
+              </Link>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  }
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  }
+                >
+                  About
+                </NavLink>
+                <div className="relative">
+                  <button
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none"
+                  >
+                    Services
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
+                      <Link
+                        to="/Reservation"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Service 1
+                      </Link>
+                      <Link
+                        to="/Menu"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Service 2
+                      </Link>
+                      <Link
+                        to="/play"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Service 3
+                      </Link>
+                      <Link
+                        to="/giftcards"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Service 3
+                      </Link>
+                    </div>
+                  )}
+                </div>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  }
+                >
+                  Contact
+                </NavLink>
+                <a
+                  href="https://squareup.com/gift/SWP5P3242C38Q/order"
+                  rel="noopener noreferrer"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Gift Cards
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div
-        className={
-          nav
-            ? "z-30 text-gray-300 fixed h-[50%] left-0 top-0 w-full bg-[#202121] ease-in-out"
-            : "fixed left-[100%]"
-        }
-      >
-        <h1></h1>
-        <ul>
-          <li>
-            <NavLink to="/" activeClassName="selected">
-              Our Story
+      {isOpen && (
+        <div className="md:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              }
+            >
+              Home
             </NavLink>
-          </li>
-          <li>
-            <NavLink to="/" activeClassName="selected">
-              {" "}
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              }
+            >
               About
             </NavLink>
-          </li>
-          <li>
-            <NavLink to="/" activeClassName="selected">
-              {" "}
-              Portfolio{" "}
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium w-full text-left"
+            >
+              Services
+            </button>
+            {isDropdownOpen && (
+              <div className="pl-4">
+                <Link
+                  to="/services/1"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Service 1
+                </Link>
+                <Link
+                  to="/services/2"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Service 2
+                </Link>
+                <Link
+                  to="/services/3"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Service 3
+                </Link>
+              </div>
+            )}
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              }
+            >
+              Contact
             </NavLink>
-          </li>
-        </ul>
-      </div>
-    </div>
+            <a
+              href="https://squareup.com/gift/SWP5P3242C38Q/order"
+              rel="noopener noreferrer"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Gift Cards
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 };
 
-export default Header;
+export default Navbar;
