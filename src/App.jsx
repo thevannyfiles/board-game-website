4eaec7ff8942a450e61f4192455ef355bced37c7
Darnel Castor <darnelcastor2@gmail.com>
2024-07-11 22:11:55 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import Header from "./Components/Header.jsx";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <Header />
+      </div>
     </>
   );
 }
